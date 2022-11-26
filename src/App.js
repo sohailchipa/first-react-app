342f3c232a0846bc6e8e1b36224877b522f0334a
sohailchipa <shoibchhipa93@gmail.com>
2022-11-26 15:32:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import Error from './pages/error';
+import Home from './pages/home';
+import About from './pages/about';
+import Contact from './pages/contact';
+
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} >
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+
+}
+
+export default App;
+
---
+++ src/layout/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WebLayout = () => {
+    return (
+        <div>WebLayout</div>
+    )
+}
+
+export default WebLayout
